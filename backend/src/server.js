32d3e7a2a316ec7175f9ea8c8145a0188fa691f5
lavanya013-lbs <slavanyathiruvaranga@gmail.com>
2026-03-22 15:24:31 +0530

    fixed sendFile path

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,14 +16,17 @@ const PORT=process.env.PORT || 3000;
 app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoutes)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join("../frontend","dist","index.html"))
-    })
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 
 
