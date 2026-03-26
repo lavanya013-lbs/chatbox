0ab34fba362725123753614f8f678814098f4243
lavanya013-lbs <slavanyathiruvaranga@gmail.com>
2026-03-26 14:38:46 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,7 +76,11 @@ export const signup= async(req,res)=>{
     }
 
 export const login=async(req,res)=>{
-    const {email,password}=req.body
+    const {email,password}=req.body;
+    if(!email || password){
+        return res.status(400).json({message:"Email and password ar required"});
+        
+    }
     try{
         const user=await User.findOne({email})
         if(!user) return res.status(400).json({message:"Invalid Credentials"})
