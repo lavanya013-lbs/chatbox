9f5ec0da9a6fcd1129cd6a28140004ca9032efa4
lavanya013-lbs <slavanyathiruvaranga@gmail.com>
2026-04-28 11:30:21 +0530

    https error fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
+      ? "https://localhost:3000/api"
       : import.meta.env.VITE_API_URL + "/api",   // ✅ FIX
   withCredentials: true,
 });
