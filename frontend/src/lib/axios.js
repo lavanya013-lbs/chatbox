2133eb64a27aa7773df4446823daf59913350984
lavanya013-lbs <slavanyathiruvaranga@gmail.com>
2026-04-29 13:24:07 +0530

    refixed error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://localhost:3000/api"
+      ? "http://localhost:3000/api"
       : import.meta.env.VITE_API_URL + "/api",   // ✅ FIX
   withCredentials: true,
 });
