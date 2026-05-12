2e1e4581240d4663614778653c57e0c0937276fd
Lavanya S <slavanyathiruvaranga@gmail.com>
2026-05-12 20:39:41 +0530

    Update cookie settings for JWT

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,8 +15,8 @@ export const generateToken=(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,//ms
         httpOnly:true,//prevents XSS attacks
-        sameSite:"strict",//CSRF attacks
-        secure:ENV.NODE_ENV==="development"?false:true,
+        sameSite:"none",//CSRF attacks
+        secure:true,
     })
     return token
 }
